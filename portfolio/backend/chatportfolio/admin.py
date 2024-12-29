12b38ca52b9d9d16a933a9f8d73eb6ba8db1cdcf
Katie House <katiehouse3@gmail.com>
2024-12-28 22:17:16 -0500

    update database django

--- portfolio/backend/chatportfolio/admin.py
+++ portfolio/backend/chatportfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Usermessage, Botresponse
 
-# Register your models here.
+
+# Register the models
+class UsermessageAdmin(admin.ModelAdmin):
+    list_display = ("created_at", "message")
+
+
+class BotresponseAdmin(admin.ModelAdmin):
+    list_display = ("created_at", "message_id", "response")
+
+
+admin.site.register(Usermessage, UsermessageAdmin)
+admin.site.register(Botresponse, BotresponseAdmin)
--- portfolio/backend/chatportfolio/models.py
+++ portfolio/backend/chatportfolio/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Usermessage(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length=500)
+
+    # string representation of the class
+    def __str__(self):
+
+        #it will return the title
+        return self.message
+
+
+class Botresponse(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    message_id = models.ForeignKey(Usermessage, on_delete=models.CASCADE)
+    response = models.CharField(max_length=500)
+
+    # string representation of the class
+    def __str__(self):
+
+        #it will return the title
+        return self.response
