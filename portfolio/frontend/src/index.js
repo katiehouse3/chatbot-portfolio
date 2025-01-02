b5ca9a74964fe104531084f870876804e8887e5a
Katie House <katiehouse3@gmail.com>
2025-01-01 20:50:24 -0500

    update react app

--- portfolio/frontend/src/index.js
+++ portfolio/frontend/src/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBot } from "botui"
+import React, { useEffect } from "react"
+import { createRoot } from "react-dom/client"
+import { BotUI, BotUIAction, BotUIMessageList } from "@botui/react"
 
-import "bootstrap/dist/css/bootstrap.min.css"; 
+import "@botui/react/dist/styles/default.theme.scss"
 
+const mybot = createBot()
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  useEffect(() => {
+    mybot.message
+      .add({ text: "Hello" })
+      .then(() => mybot.wait({ waitTime: 1000 }))
+      .then(() => mybot.message.add({ text: "how are you?" }))
+      .then(() => mybot.wait({ waitTime: 500 }))
+      .then(() =>
+        mybot.action.set({ placeholder: 'enter your name' }, { actionType: 'input' })
+      )
+      .then((data) => mybot.wait({ waitTime: 500 }, data))
+      .then((data) => mybot.message.add({ text: `You are feeling ${data.text}!` })
+      ).then((data) => console.log(data.text))
+  }, [])
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div>
+      <BotUI bot={mybot}>
+        <BotUIMessageList />
+        <BotUIAction />
+      </BotUI>
+    </div>
+  )
+}
+
+const containerElement = document.getElementById("botui")
+if (containerElement) {
+  const root = createRoot(containerElement)
+  root.render(<App />)
+}
