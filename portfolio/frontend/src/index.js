7caacabdab186fd29b5c5578eb91e44f18c2bd06
Katie House <katiehouse3@gmail.com>
2025-02-03 20:00:25 -0500

    fix loop issue

--- portfolio/frontend/src/index.js
+++ portfolio/frontend/src/index.js
@@ -10,7 +10,6 @@ import './index.css';
 
 const glasses = findIconDefinition({ prefix: 'fas', iconName: 'glasses' })
 
-
 const mybot = createBot()
 
 function chatbotLoop() {
@@ -27,26 +26,31 @@ function chatbotLoop() {
         console.log(response.data.response)
         mybot.next()
         mybot.message.add({ text: `${response.data.response}` }, data)
+        chatbotLoop(); 
       })
       .catch(function (error) {
         console.log(error);
       })
   })
-  setTimeout(chatbotLoop, 1000); 
 }
 
-const App = () => {
-  useEffect(() => {
-    mybot.message
+const runBot = () => {
+  mybot.message
       .add({ text: "Hello" })
       .then(() => mybot.wait({ waitTime: 1000 }))
       .then(() => mybot.message.add({ text: "Welcome to my portfolio!" }))
       .then(() => mybot.wait({ waitTime: 500 }))
       .then(() => mybot.message.add({ text: "I am a Data Scientist with experience building end-to-end machine learning solutions in generative AI, marketing funnel optimization, advisor success and more. What would you like to learn about me?" }))
       .then(() => mybot.wait({ waitTime: 500 }))
-      .then(() => chatbotLoop())
+      .then(() => chatbotLoop(mybot))
+}
 
-  }, [])
+const App = () => {
+  
+  
+  useEffect(() => {
+    runBot(mybot)
+  })
 
   return (
     <div>
