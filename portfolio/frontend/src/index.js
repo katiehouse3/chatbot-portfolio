5523503c2933e4116f127441b87fa180ec3462a6
Katie House <katiehouse3@gmail.com>
2025-02-17 13:32:43 -0500

    add some error handling

--- portfolio/frontend/src/index.js
+++ portfolio/frontend/src/index.js
@@ -18,6 +18,17 @@ const mybot = createBot()
 
 function getResponse(data) {
   mybot.wait()
+  console.log(data.text.length)
+  if (data.text.length < 2 || /^\d+$/.test(data.text)) {
+    mybot.message.add({ text: "Please enter at least 2 characters and include non-numeric values." }, data)
+    chatbotLoop()
+    return
+  }
+  if (/[\{\}\[\]\(\)\<\>\=\&]/.test(data.text)) {
+    mybot.message.add({ text: "Code snippets are not allowed. Please enter a valid query." }, data)
+    chatbotLoop()
+    return
+  }
   axios.post('http://localhost:8000/api/botmessages/', { query: data.text })
     .then(function (response) {
       const response_text=response.data.response.toString();
@@ -37,6 +48,8 @@ function chatBotExample(){
       options: [
         { label: 'What are some of Katie\'s skills?', value: 'What are some of Katie\'s skills?' },
         { label: 'How much value has Katie generated at MassMutual?', value: 'How much value has Katie generated at MassMutual?' },
+        { label: 'What programming languages does Katie know?', value: 'What programming languages does Katie know?' },
+        { label: 'What are some of Katie\'s extracurriculars?', value: 'What are some of Katie\'s extracurriculars?' },
       ],
     },
     {
@@ -70,7 +83,7 @@ const runBot = () => {
     .then(() => mybot.wait({ waitTime: 1000 }))
     .then(() => mybot.message.add({ text: <Markdown>{"Welcome to my portfolio! I am a **Data Scientist** with experience building **end-to-end machine learning solutions** in generative AI, marketing funnel optimization, advisor success and more. I built this **AI chatbot** to answer questions about my experience." }</Markdown>}))
     .then(() => mybot.wait({ waitTime: 500 }))
-    .then(() => mybot.message.add({ text: <Markdown>{"Try an example question to get started:"}</Markdown>}))
+    .then(() => mybot.message.add({ text: <Markdown>{"Everything is logged in a database. **Do not submit personal information in queries.** Try an **example question** to get started:"}</Markdown>}))
     .then(() => mybot.wait({ waitTime: 500 }))
     .then(() => chatBotExample(mybot))
 }
